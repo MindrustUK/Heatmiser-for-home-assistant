de21d9c0ee63ca1ac00e3afda809e3fca8d8c35f
MindrustUK <mehmetcan@mindrust.co.uk>
2018-11-25 20:52:40 +0000

    Update heatmiserneo.py

--- heatmiserneo.py
+++ heatmiserneo.py
@@ -78,7 +78,7 @@ class HeatmiserNeostat(ClimateDevice):
         self._host = host
         self._port = port
         #self._type = type Neostat vs Neostat-e
-        self._operation = "STATE_IDLE"
+        self._operation = STATE_IDLE
         self.update()
 
     @property

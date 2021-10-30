76fd0ccd006c5175b9243bef9ef25ef2bae6749c
ribbal <30695106+ribbal@users.noreply.github.com>
2021-10-30 18:06:29 +0100

    Don't add sensor if floor temp probe not connected

--- custom_components/heatmiserneo/sensor.py
+++ custom_components/heatmiserneo/sensor.py
@@ -27,13 +27,13 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     hub = hass.data[DOMAIN][HUB]
     _, devices = await hub.get_live_data()
-
+    
     system_data = await hub.get_system()
     temperature_unit = system_data.CORF
 
     sensors = [
         HeatmiserNeostatFloorTemperature(hub, sensor, temperature_unit)
-        for sensor in devices["thermostats"]
+        for sensor in devices["thermostats"] if sensor.current_floor_temperature < 127  # Exclude device if floor temperature is 127 or higher (V1 of the NeoStat reports 127 when no probe is connected, V2 reports 127.5)
     ]
 
     _LOGGER.info("Adding Thermostats Sensors: %s " % sensors)
@@ -48,7 +48,7 @@ class HeatmiserNeostatFloorTemperature(SensorEntity):
         self._hub = hub
         self._unit_of_measurement = unit_of_measurement
         self._state = None
-
+        
     @property
     def name(self):
         """Return the name of the sensor."""
@@ -66,7 +66,7 @@ class HeatmiserNeostatFloorTemperature(SensorEntity):
 
     @property
     def entity_registry_enabled_default(self):
-        """Disable entity if vaule is above 127 (V1 of the NeoStat reports 127 when no probe is connected, V2 reports 127.5)"""
+        """Disable entity if value is above 127 (V1 of the NeoStat reports 127 when no probe is connected, V2 reports 127.5)"""
         return self.state < 127
 
     @property
@@ -95,7 +95,7 @@ class HeatmiserNeostatFloorTemperature(SensorEntity):
 
     async def async_update(self):
         """Update the sensor's status."""
-        _LOGGER.debug("Entered update(self)")
+        _LOGGER.debug("Entered sensor.update(self)")
         _, devices = await self._hub.get_live_data()
         for sensor in devices["thermostats"]:
             if self._neostat.name == sensor.name:

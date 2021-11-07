d4de0299f69fede85db0bdf641cad8687936c55f
ribbal <30695106+ribbal@users.noreply.github.com>
2021-11-07 13:51:49 +0000

    Further performance improvements
    Move update_data() from individual entity files to init.py so that the hub is only called once

--- custom_components/heatmiserneo/__init__.py
+++ custom_components/heatmiserneo/__init__.py
@@ -1,10 +1,19 @@
 # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
+import logging
+from datetime import timedelta
+import async_timeout
+
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
 
 from homeassistant.const import CONF_HOST,CONF_PORT
 
 from neohubapi.neohub import NeoHub
-from .const import DOMAIN, HUB
+from .const import DOMAIN, HUB, COORDINATOR
 
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
     """Set up Heamiser Neo components."""
@@ -21,6 +30,35 @@ async def async_setup_entry(hass, entry):
     hub = NeoHub(host, port)
     hass.data[DOMAIN][HUB] = hub
 
+    async def async_update_data():
+        """Fetch data from the Hub all at once and make it available for
+           all thermostats.
+        """
+        _LOGGER.info(f"Executing update_data()")
+        
+        async with async_timeout.timeout(30):
+            _, devices_data = await hub.get_live_data()
+            system_data = await hub.get_system()
+            
+            #_LOGGER.debug(f"system_data: {system_data}")
+            _LOGGER.debug(f"devices_data: {devices_data}")
+            
+            return (devices_data, system_data)
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        # Name of the data. For logging purposes.
+        name="neostat",
+        update_method=async_update_data,
+        # Polling interval. Will only be polled if there are subscribers.
+        update_interval=timedelta(seconds=30)
+    )
+
+    hass.data[DOMAIN][COORDINATOR] = coordinator
+
+    await coordinator.async_config_entry_first_refresh()
+    
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "climate")
     )

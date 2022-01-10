91482634cc13748bd37a5f9def8d28126abfa2bf
ribbal <30695106+ribbal@users.noreply.github.com>
2022-01-10 21:50:37 +0000

    Add options configuration

--- custom_components/heatmiserneo/const.py
+++ custom_components/heatmiserneo/const.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
 
+import enum
+
 """Constants used by multiple Heatmiser Neo modules."""
 DOMAIN = "heatmiserneo"
 
@@ -8,3 +10,12 @@ COORDINATOR = "Coordinator"
 
 DEFAULT_HOST = "Neo-Hub"
 DEFAULT_PORT = 4242
+
+CONF_HVAC_MODES = "hvac_modes"
+
+# This should be in the neohubapi.neohub enums code
+class AvailableMode(str, enum.Enum):
+    HEAT = "heat"
+    COOL = "cool"
+    VENT = "vent"
+    AUTO = "auto"

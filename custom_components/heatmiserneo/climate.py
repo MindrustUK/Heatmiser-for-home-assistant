6ca5ddae1deaa0b038a96595e86cdf9d5a2b5fe1
Ruben d'Arco <cyclops-github@prof-x.net>
2023-04-27 19:57:42 +0000

    better support for battery
    This allows us to see the battery status in the device overview.

--- custom_components/heatmiserneo/climate.py
+++ custom_components/heatmiserneo/climate.py
@@ -1,4 +1,3 @@
-
 # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
 
 """
@@ -10,7 +9,7 @@ Heatmiser NeoStat control via Heatmiser Neo-hub
 
 import logging
 import asyncio
- 
+
 from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import (
     ATTR_TARGET_TEMP_HIGH,
@@ -24,7 +23,7 @@ from homeassistant.components.climate.const import (
     HVAC_MODE_HEAT_COOL,
     HVAC_MODE_OFF,
     SUPPORT_TARGET_TEMPERATURE,
-    SUPPORT_TARGET_TEMPERATURE_RANGE
+    SUPPORT_TARGET_TEMPERATURE_RANGE,
 )
 from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS, TEMP_FAHRENHEIT
 from homeassistant.helpers.update_coordinator import (
@@ -39,61 +38,76 @@ _LOGGER = logging.getLogger(__name__)
 
 
 SUPPORT_FLAGS = 0
-THERMOSTATS = 'thermostats'
+THERMOSTATS = "thermostats"
 
 
 hvac_mode_mapping = {
-    AvailableMode.AUTO: HVAC_MODE_HEAT_COOL, 
-    AvailableMode.COOL: HVAC_MODE_COOL, 
-    AvailableMode.VENT: HVAC_MODE_FAN_ONLY, 
-    AvailableMode.HEAT: HVAC_MODE_HEAT
+    AvailableMode.AUTO: HVAC_MODE_HEAT_COOL,
+    AvailableMode.COOL: HVAC_MODE_COOL,
+    AvailableMode.VENT: HVAC_MODE_FAN_ONLY,
+    AvailableMode.HEAT: HVAC_MODE_HEAT,
 }
 
-async def async_setup_entry(hass, entry, async_add_entities):
 
+async def async_setup_entry(hass, entry, async_add_entities):
     hub: NeoHub = hass.data[DOMAIN][HUB]
     coordinator: DataUpdateCoordinator = hass.data[DOMAIN][COORDINATOR]
 
     (devices_data, system_data) = coordinator.data
-    thermostats = {device.name : device for device in devices_data[THERMOSTATS]}
-    
-    hvac_config = entry.options[CONF_HVAC_MODES] if CONF_HVAC_MODES in entry.options else {}
+    thermostats = {device.name: device for device in devices_data[THERMOSTATS]}
+
+    hvac_config = (
+        entry.options[CONF_HVAC_MODES] if CONF_HVAC_MODES in entry.options else {}
+    )
     _LOGGER.debug(f"hvac_config: {hvac_config}")
     for config in hvac_config:
-        _LOGGER.debug(f"Overriding the default HVAC modes from {thermostats[config].available_modes} to {hvac_config[config]} for the {config} climate entity.")
+        _LOGGER.debug(
+            f"Overriding the default HVAC modes from {thermostats[config].available_modes} to {hvac_config[config]} for the {config} climate entity."
+        )
         thermostats[config].available_modes = hvac_config[config]
-    
+
     temperature_unit = system_data.CORF
     temperature_step = await hub.target_temperature_step
 
-    entities = [NeoStatEntity(thermostat, coordinator, temperature_unit, temperature_step) for thermostat in thermostats.values()]
-    
+    entities = [
+        NeoStatEntity(thermostat, coordinator, temperature_unit, temperature_step)
+        for thermostat in thermostats.values()
+    ]
+
     _LOGGER.info(f"Adding Thermostats: {entities}")
     async_add_entities(entities, True)
-    
+
+
 class NeoStatEntity(CoordinatorEntity, ClimateEntity):
-    """ Represents a Heatmiser neoStat thermostat. """
-    def __init__(self, neostat: NeoStat, coordinator: DataUpdateCoordinator, unit_of_measurement, temperature_step):
+    """Represents a Heatmiser neoStat thermostat."""
+
+    def __init__(
+        self,
+        neostat: NeoStat,
+        coordinator: DataUpdateCoordinator,
+        unit_of_measurement,
+        temperature_step,
+    ):
         super().__init__(coordinator)
         _LOGGER.debug(f"Creating {neostat}")
-        
+
         self._neostat = neostat
         self._coordinator = coordinator
         self._unit_of_measurement = unit_of_measurement
         self._target_temperature_step = temperature_step
         self._hvac_modes = []
-        if hasattr(neostat, 'standby'):
+        if hasattr(neostat, "standby"):
             self._hvac_modes.append(HVAC_MODE_OFF)
         for mode in neostat.available_modes:
             self._hvac_modes.append(hvac_mode_mapping[mode])
-      
+
     @property
     def data(self):
-        """Helper to get the data for the current thermostat. """
+        """Helper to get the data for the current thermostat."""
         (devices, _) = self._coordinator.data
-        thermostats = {device.name : device for device in devices[THERMOSTATS]}
+        thermostats = {device.name: device for device in devices[THERMOSTATS]}
         return thermostats[self.name]
-        
+
     @property
     def supported_features(self):
         """Return the list of supported features."""
@@ -111,15 +125,15 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
         else:
             _LOGGER.error(f"Unsupported hvac mode: {hvac_mode}")
             return SUPPORT_FLAGS
-            
+
     @property
     def should_poll(self):
-        """ Don't poll - we fetch the data from the hub all at once """
+        """Don't poll - we fetch the data from the hub all at once"""
         return False
 
     @property
     def name(self):
-        """ Returns the name. """
+        """Returns the name."""
         return self._neostat.name
 
     @property
@@ -135,10 +149,10 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
         if self._unit_of_measurement == "F":
             return TEMP_FAHRENHEIT
         return self._unit_of_measurement
-        
+
     @property
     def current_temperature(self):
-        """ Returns the current temperature. """
+        """Returns the current temperature."""
         return float(self.data.temperature)
 
     @property
@@ -160,18 +174,18 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
     def target_temperature_step(self):
         """Return the supported step of target temperature."""
         return self._target_temperature_step
-         
+
     @property
     def extra_state_attributes(self):
         """Return the additional state attributes."""
         attributes = {}
-        attributes['low_battery'] = self.data.low_battery
-        attributes['offline'] = self.data.offline
-        attributes['standby'] = self.data.standby
-        attributes['floor_temperature'] = self.data.current_floor_temperature
-        attributes['preheat_active'] = bool(self.data.preheat_active)
+        attributes["low_battery"] = self.data.low_battery
+        attributes["offline"] = self.data.offline
+        attributes["standby"] = self.data.standby
+        attributes["floor_temperature"] = self.data.current_floor_temperature
+        attributes["preheat_active"] = bool(self.data.preheat_active)
         return attributes
-    
+
     @property
     def hvac_action(self):
         """Return current activity ie. currently heating, cooling, idle."""
@@ -181,28 +195,31 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
             return CURRENT_HVAC_COOL
         else:
             return CURRENT_HVAC_IDLE
-            
+
     @property
     def hvac_mode(self):
         """Return current operation mode ie. heat, cool, off."""
         if self.data.standby or not self.data.hc_mode:
             return HVAC_MODE_OFF
-            
+
         hc_mode = HCMode(self.data.hc_mode)
         if hc_mode == HCMode.AUTO and AvailableMode.AUTO in self.data.available_modes:
             return HVAC_MODE_HEAT_COOL
         elif hc_mode == HCMode.VENT and AvailableMode.VENT in self.data.available_modes:
             return HVAC_MODE_FAN_ONLY
-        elif hc_mode == HCMode.COOLING and AvailableMode.COOL in self.data.available_modes:
+        elif (
+            hc_mode == HCMode.COOLING
+            and AvailableMode.COOL in self.data.available_modes
+        ):
             return HVAC_MODE_COOL
         else:
             return HVAC_MODE_HEAT
-            
+
     @property
     def hvac_modes(self):
         """Return the list of available operation modes."""
         return self._hvac_modes
-    
+
     @property
     def device_info(self):
         return {
@@ -214,22 +231,30 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
         }
 
     async def async_set_temperature(self, **kwargs):
-        """ Set new target temperature. """
+        """Set new target temperature."""
         _LOGGER.info(f"{self.name} : Executing set_temperature() with: {kwargs}")
         _LOGGER.debug(f"self.data: {self.data}")
-        
+
         low_temp = kwargs.get(ATTR_TEMPERATURE) or kwargs.get(ATTR_TARGET_TEMP_LOW)
         high_temp = kwargs.get(ATTR_TARGET_TEMP_HIGH)
 
-        set_target_temperature_task = asyncio.create_task(self._neostat.set_target_temperature(low_temp))
+        set_target_temperature_task = asyncio.create_task(
+            self._neostat.set_target_temperature(low_temp)
+        )
         response = await set_target_temperature_task
         if response:
-            _LOGGER.info(f"{self.name} : Called set_target_temperature with: {low_temp} (response: {response})")
-        
-        set_target_cool_temperature_task = asyncio.create_task(self._neostat.set_cool_temp(high_temp))
+            _LOGGER.info(
+                f"{self.name} : Called set_target_temperature with: {low_temp} (response: {response})"
+            )
+
+        set_target_cool_temperature_task = asyncio.create_task(
+            self._neostat.set_cool_temp(high_temp)
+        )
         response = await set_target_cool_temperature_task
         if response:
-            _LOGGER.info(f"{self.name} : Called set_cool_temp with: {high_temp} (response: {response})")
+            _LOGGER.info(
+                f"{self.name} : Called set_cool_temp with: {high_temp} (response: {response})"
+            )
 
         # The change of target temperature may trigger a change in the current hvac_action
         # so we schedule a refresh to get new data asap.
@@ -239,7 +264,7 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
         """Set hvac mode."""
         _LOGGER.info(f"{self.name} : Executing set_hvac_mode() with: {hvac_mode}")
         _LOGGER.debug(f"self.data: {self.data}")
-        
+
         hc_mode: HCMode = None
         if hvac_mode == HVAC_MODE_HEAT:
             hc_mode = HCMode.HEATING
@@ -258,9 +283,13 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
         if hc_mode:
             set_hc_mode_task = asyncio.create_task(self._neostat.set_hc_mode(hc_mode))
             response = await set_hc_mode_task
-            _LOGGER.info(f"{self.name} : Called set_hc_mode() with: {hc_mode} (response: {response})")
+            _LOGGER.info(
+                f"{self.name} : Called set_hc_mode() with: {hc_mode} (response: {response})"
+            )
 
         frost: bool = True if hvac_mode == HVAC_MODE_OFF else False
         set_frost_task = asyncio.create_task(self._neostat.set_frost(frost))
         response = await set_frost_task
-        _LOGGER.info(f"{self.name} : Called set_frost() with: {frost} (response: {response})")
+        _LOGGER.info(
+            f"{self.name} : Called set_frost() with: {frost} (response: {response})"
+        )
--- custom_components/heatmiserneo/sensor.py
+++ custom_components/heatmiserneo/sensor.py
@@ -1,4 +1,3 @@
-
 # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
 
 """
@@ -14,12 +13,14 @@ from abc import ABCMeta, abstractmethod
 
 from homeassistant.components.binary_sensor import (
     BinarySensorEntity,
-    DEVICE_CLASS_BATTERY,
-    DEVICE_CLASS_PROBLEM
+    DEVICE_CLASS_PROBLEM,
 )
 
+from homeassistant.components.sensor import SensorEntity, DEVICE_CLASS_BATTERY
+
 from homeassistant.helpers.update_coordinator import (
-    DataUpdateCoordinator, CoordinatorEntity
+    DataUpdateCoordinator,
+    CoordinatorEntity,
 )
 
 from neohubapi.neohub import NeoHub, NeoStat
@@ -27,35 +28,40 @@ from .const import DOMAIN, HUB, COORDINATOR
 
 _LOGGER = logging.getLogger(__name__)
 
-THERMOSTATS = 'thermostats'
-LOW_BATTERY_ICON = "mdi:battery-low"
-OFFLINE_ICON = "mdi:network-off-outline"
+THERMOSTATS = "thermostats"
+ICON_BATTERY_LOW = "mdi:battery-low"
+ICON_BATTERY_OFF = "mdi:battery-off"
+ICON_BATTERY_FULL = "mdi:battery"
+ICON_NETWORK_OFFLINE = "mdi:network-off-outline"
+ICON_NETOWRK_ONLINE = "mdi:network-outline"
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
-
-    hub: NeoHub = hass.data[DOMAIN][HUB]
+    # hub: NeoHub = hass.data[DOMAIN][HUB]
     coordinator: DataUpdateCoordinator = hass.data[DOMAIN][COORDINATOR]
 
     (devices_data, system_data) = coordinator.data
     thermostats = {device.name: device for device in devices_data[THERMOSTATS]}
 
-    low_battery_binary_sensors = []
+    battery_level_sensors = []
     offline_binary_sensors = []
 
     for thermostat in thermostats.values():
-        low_battery_binary_sensors.append(NeoStatLowBatteryBinarySensor(thermostat, coordinator))
-        offline_binary_sensors.append(NeoStatOfflineBinarySensor(thermostat, coordinator))
+        battery_level_sensors.append(NeoStatBatterySensor(thermostat, coordinator))
+        offline_binary_sensors.append(
+            NeoStatOfflineBinarySensor(thermostat, coordinator)
+        )
 
-    _LOGGER.info(f"Adding Thermostat Low Battery Binary Sensors: {low_battery_binary_sensors}")
-    async_add_entities(low_battery_binary_sensors, True)
+    _LOGGER.info(
+        f"Adding Thermostat Low Battery Binary Sensors: {battery_level_sensors}"
+    )
+    async_add_entities(battery_level_sensors, True)
     _LOGGER.info(f"Adding Thermostat Offline Binary Sensors: {offline_binary_sensors}")
     async_add_entities(offline_binary_sensors, True)
 
 
-class NeoStatBinarySensor(CoordinatorEntity, BinarySensorEntity):
-    """ Represents a Heatmiser Neostat binary sensor """
-    __metaclass__ = ABCMeta
+class NeoStatBatterySensor(CoordinatorEntity, SensorEntity):
+    """Represents the battery status of the thermostaat"""
 
     def __init__(self, neostat: NeoStat, coordinator: DataUpdateCoordinator):
         super().__init__(coordinator)
@@ -65,49 +71,49 @@ class NeoStatBinarySensor(CoordinatorEntity, BinarySensorEntity):
         self._coordinator = coordinator
 
     @property
-    @abstractmethod
-    def name(self):
-        """Return the name of the sensor."""
-        pass
-
-    @property
-    @abstractmethod
-    def unique_id(self):
-        """Return a unique ID"""
-        pass
-
-    @property
     def data(self):
-        """Helper to get the data for the current thermostat. """
+        """Helper to get the data for the current thermostat."""
         (devices, _) = self._coordinator.data
         thermostats = {device.name: device for device in devices[THERMOSTATS]}
         return thermostats[self._neostat.name]
 
     @property
-    def should_poll(self):
-        """ Don't poll - we fetch the data from the hub all at once """
-        return False
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{self._neostat.name} Battery Level Sensor"
 
     @property
-    def available(self):
-        """Return true if the entity is available."""
-        return True
+    def unique_id(self):
+        """Return a unique ID"""
+        return f"{self._neostat.device_id}_battery_level"
 
     @property
-    @abstractmethod
-    def is_on(self):
-        """Return true if the binary sensor is on."""
-        pass
+    def native_unit_of_measurement(self):
+        """Return the percentage"""
+        return "%"
 
     @property
-    @abstractmethod
+    def native_value(self):
+        """Return the battery value in percentage."""
+        if self.data.offline:
+            return 0
+        if self.data.low_battery:
+            return 10
+
+        return 100
+
+    @property
     def device_class(self):
-        pass
+        return DEVICE_CLASS_BATTERY
 
     @property
-    @abstractmethod
     def icon(self):
-        pass
+        if self.data.offline:
+            return ICON_BATTERY_OFF
+        if self.data.low_battery:
+            return ICON_BATTERY_LOW
+
+        return ICON_BATTERY_FULL
 
     @property
     def device_info(self):
@@ -120,45 +126,37 @@ class NeoStatBinarySensor(CoordinatorEntity, BinarySensorEntity):
         }
 
 
-class NeoStatLowBatteryBinarySensor(NeoStatBinarySensor):
-    """ Represents a Heatmiser Neostat low battery binary sensor """
+class NeoStatOfflineBinarySensor(CoordinatorEntity, BinarySensorEntity):
+    """Represents a Heatmiser Neostat offline binary sensor"""
 
     def __init__(self, neostat: NeoStat, coordinator: DataUpdateCoordinator):
-        super().__init__(neostat, coordinator)
+        super().__init__(coordinator)
+        _LOGGER.debug(f"Creating {type(self).__name__} for {neostat.name}")
+
+        self._neostat = neostat
+        self._coordinator = coordinator
 
     @property
-    def name(self):
-        """Return the name of the sensor."""
-        return f"{self._neostat.name} Low Battery Binary Sensor"
-
-    @property
-    def unique_id(self):
-        """Return a unique ID"""
-        return f"{self._neostat.device_id}_low_battery"
-
-    @property
-    def is_on(self) -> Optional[bool]:
-        """Return true if the binary sensor is on. i.e. NeoStat battery is low"""
-        return self._neostat.low_battery
-
-    @property
-    def device_class(self):
-        return DEVICE_CLASS_BATTERY
-
-    @property
-    def icon(self):
-        return LOW_BATTERY_ICON
-
-
-class NeoStatOfflineBinarySensor(NeoStatBinarySensor):
-    """ Represents a Heatmiser Neostat offline binary sensor """
-    def __init__(self, neostat: NeoStat, coordinator: DataUpdateCoordinator):
-        super().__init__(neostat, coordinator)
+    def data(self):
+        """Helper to get the data for the current thermostat."""
+        (devices, _) = self._coordinator.data
+        thermostats = {device.name: device for device in devices[THERMOSTATS]}
+        return thermostats[self._neostat.name]
 
     @property
     def name(self):
         """Return the name of the sensor."""
         return f"{self._neostat.name} Offline Binary Sensor"
+
+    @property
+    def should_poll(self):
+        """Don't poll - we fetch the data from the hub all at once"""
+        return False
+
+    @property
+    def available(self):
+        """Return true if the entity is available."""
+        return True
 
     @property
     def unique_id(self):
@@ -176,4 +174,17 @@ class NeoStatOfflineBinarySensor(NeoStatBinarySensor):
 
     @property
     def icon(self):
-        return OFFLINE_ICON
+        if self.data.offline:
+            return ICON_NETWORK_OFFLINE
+
+        return ICON_NETOWRK_ONLINE
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {("heatmiser neoStat", self._neostat.name)},
+            "name": self._neostat.name,
+            "manufacturer": "Heatmiser",
+            "model": "neoStat",
+            "suggested_area": self._neostat.name,
+        }

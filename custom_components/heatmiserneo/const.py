3e5157cd01944c4538a0566178e1f8252c81fcfd
MindrustUK <mehmetcan@mindrust.co.uk>
2023-11-21 19:56:15 +0000

    Update const.py

--- custom_components/heatmiserneo/const.py
+++ custom_components/heatmiserneo/const.py
@@ -1,26 +1,32 @@
-# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
+    # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
 
-import enum
+    import enum
 
-"""Constants used by multiple Heatmiser Neo modules."""
-DOMAIN = "heatmiserneo"
+    """Constants used by multiple Heatmiser Neo modules."""
+    DOMAIN = "heatmiserneo"
 
-HUB = "Hub"
-COORDINATOR = "Coordinator"
+    HUB = "Hub"
+    COORDINATOR = "Coordinator"
 
-DEFAULT_HOST = "Neo-Hub"
-DEFAULT_PORT = 4242
+    DEFAULT_HOST = "Neo-Hub"
+    DEFAULT_PORT = 4242
+    DEFAULT_TOKEN = ""
 
-CONF_HVAC_MODES = "hvac_modes"
+    CONF_HVAC_MODES = "hvac_modes"
 
-SERVICE_HOLD_ON = "hold_on"
-SERVICE_HOLD_OFF = "hold_off"
-ATTR_HOLD_DURATION = "hold_duration"
-ATTR_HOLD_TEMPERATURE = "hold_temperature"
+    SERVICE_HOLD_ON = "hold_on"
+    SERVICE_HOLD_OFF = "hold_off"
+    ATTR_HOLD_DURATION = "hold_duration"
+    ATTR_HOLD_TEMPERATURE = "hold_temperature"
 
-# This should be in the neohubapi.neohub enums code
-class AvailableMode(str, enum.Enum):
-    HEAT = "heat"
-    COOL = "cool"
-    VENT = "vent"
-    AUTO = "auto"
+    HEATMISER_PRODUCT_LIST = ["NULL", "NeoStat V1", "SmartStat", "CoolSwitch", "TCM RH", "Contact Sensor", "Neo Plug",
+                              "NeoAir", "SmartStat HC", "NeoAir HW", "Repeater", "NeoStat HC", "NeoStat V2", "NeoAir V2",
+                              "Air Sensor", "NeoAir V2 Combo", "RF Switch Wifi", "Edge WiFi"]
+
+
+    # This should be in the neohubapi.neohub enums code
+    class AvailableMode(str, enum.Enum):
+        HEAT = "heat"
+        COOL = "cool"
+        VENT = "vent"
+        AUTO = "auto"

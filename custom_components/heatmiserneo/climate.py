94d6a70fe6888a2c873e0b846748739b06344a6d
MindrustUK <mehmetcan@mindrust.co.uk>
2023-11-21 19:48:03 +0000

    Update climate.py

--- custom_components/heatmiserneo/climate.py
+++ custom_components/heatmiserneo/climate.py
@@ -9,10 +9,23 @@ Heatmiser NeoStat control via Heatmiser Neo-hub
 
 import logging
 import asyncio
+from collections import OrderedDict
 
 import voluptuous as vol
 
-from homeassistant.components.climate import ClimateEntity
+from homeassistant.components.climate import (
+    ATTR_TARGET_TEMP_HIGH,
+    ATTR_TARGET_TEMP_LOW,
+    FAN_AUTO,
+    FAN_ON,
+    PRESET_AWAY,
+    PRESET_NONE,
+    ClimateEntity,
+    ClimateEntityFeature,
+    HVACAction,
+    HVACMode,
+)
+
 from homeassistant.components.climate.const import (
     ATTR_TARGET_TEMP_HIGH,
     ATTR_TARGET_TEMP_LOW,
@@ -66,7 +79,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
     coordinator: DataUpdateCoordinator = hass.data[DOMAIN][COORDINATOR]
 
     (devices_data, system_data) = coordinator.data
-    thermostats = {device.name: device for device in devices_data[THERMOSTATS]}
+    thermostats = {device.name: device for device in devices_data['neo_devices']}
 
     hvac_config = (
         entry.options[CONF_HVAC_MODES] if CONF_HVAC_MODES in entry.options else {}
@@ -81,10 +94,11 @@ async def async_setup_entry(hass, entry, async_add_entities):
     temperature_unit = system_data.CORF
     temperature_step = await hub.target_temperature_step
 
-    entities = [
-        NeoStatEntity(thermostat, coordinator, hub, temperature_unit, temperature_step)
-        for thermostat in thermostats.values()
-    ]
+    entities = []
+    for device in thermostats.values():
+        if (device.device_type == 1) or (device.device_type == 12):
+            if not device.time_clock_mode:
+                entities.append(NeoStatEntity(device, coordinator, hub, temperature_unit, temperature_step))
 
     _LOGGER.info(f"Adding Thermostats: {entities}")
     async_add_entities(entities, True)
@@ -106,16 +120,17 @@ async def async_setup_entry(hass, entry, async_add_entities):
         "unset_hold",
     )
 
+
 class NeoStatEntity(CoordinatorEntity, ClimateEntity):
     """Represents a Heatmiser neoStat thermostat."""
 
     def __init__(
-        self,
-        neostat: NeoStat,
-        coordinator: DataUpdateCoordinator,
-        hub: NeoHub,
-        unit_of_measurement,
-        temperature_step
+            self,
+            neostat: NeoStat,
+            coordinator: DataUpdateCoordinator,
+            hub: NeoHub,
+            unit_of_measurement,
+            temperature_step
     ):
 
         super().__init__(coordinator)
@@ -136,164 +151,8 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
     def data(self):
         """Helper to get the data for the current thermostat."""
         (devices, _) = self._coordinator.data
-        thermostats = {device.name: device for device in devices[THERMOSTATS]}
-        return thermostats[self.name]
-
-    @property
-    def supported_features(self):
-        """Return the list of supported features."""
-        hvac_mode = self.hvac_mode
-        if hvac_mode == HVAC_MODE_HEAT:
-            return SUPPORT_FLAGS | SUPPORT_TARGET_TEMPERATURE
-        elif hvac_mode == HVAC_MODE_COOL:
-            return SUPPORT_FLAGS | SUPPORT_TARGET_TEMPERATURE
-        elif hvac_mode == HVAC_MODE_OFF:
-            return SUPPORT_FLAGS
-        elif hvac_mode == HVAC_MODE_HEAT_COOL:
-            return SUPPORT_FLAGS | SUPPORT_TARGET_TEMPERATURE_RANGE
-        elif hvac_mode == HVAC_MODE_FAN_ONLY:
-            return SUPPORT_FLAGS | SUPPORT_TARGET_TEMPERATURE
-        else:
-            _LOGGER.error(f"Unsupported hvac mode: {hvac_mode}")
-            return SUPPORT_FLAGS
-
-    @property
-    def should_poll(self):
-        """Don't poll - we fetch the data from the hub all at once"""
-        return False
-
-    @property
-    def name(self):
-        """Returns the name."""
-        return self._neostat.name
-
-    @property
-    def unique_id(self):
-        """Return a unique ID."""
-        return self._neostat.name
-
-    @property
-    def temperature_unit(self):
-        """Return the unit of measurement."""
-        if self._unit_of_measurement == "C":
-            return TEMP_CELSIUS
-        if self._unit_of_measurement == "F":
-            return TEMP_FAHRENHEIT
-        return self._unit_of_measurement
-
-    @property
-    def current_temperature(self):
-        """Returns the current temperature."""
-        return float(self.data.temperature)
-
-    @property
-    def target_temperature(self):
-        """Return the temperature we try to reach."""
-        return float(self.data.target_temperature)
-
-    @property
-    def target_temperature_high(self):
-        """Return the temperature we try to reach."""
-        return float(self.data.cool_temp)
-
-    @property
-    def target_temperature_low(self):
-        """Return the temperature we try to reach."""
-        return float(self.data.target_temperature)
-
-    @property
-    def target_temperature_step(self):
-        """Return the supported step of target temperature."""
-        return self._target_temperature_step
-
-    @property
-    def extra_state_attributes(self):
-        """Return the additional state attributes."""
-        attributes = {}
-        attributes['low_battery'] = self.data.low_battery
-        attributes['offline'] = self.data.offline
-        attributes['standby'] = self.data.standby
-        attributes['hold_on'] = self.data.hold_on
-        attributes['hold_time'] = ':'.join(str(self.data.hold_time).split(':')[:2])
-        attributes['hold_temp'] = self.data.hold_temp
-        attributes['floor_temperature'] = self.data.current_floor_temperature
-        attributes['preheat_active'] = bool(self.data.preheat_active)
-
-        return attributes
-
-    @property
-    def hvac_action(self):
-        """Return current activity ie. currently heating, cooling, idle."""
-        if self.data.heat_on:
-            return CURRENT_HVAC_HEAT
-        elif self.data.cool_on:
-            return CURRENT_HVAC_COOL
-        else:
-            return CURRENT_HVAC_IDLE
-
-    @property
-    def hvac_mode(self):
-        """Return current operation mode ie. heat, cool, off."""
-        if self.data.standby or not self.data.hc_mode:
-            return HVAC_MODE_OFF
-
-        hc_mode = HCMode(self.data.hc_mode)
-        if hc_mode == HCMode.AUTO and AvailableMode.AUTO in self.data.available_modes:
-            return HVAC_MODE_HEAT_COOL
-        elif hc_mode == HCMode.VENT and AvailableMode.VENT in self.data.available_modes:
-            return HVAC_MODE_FAN_ONLY
-        elif (
-            hc_mode == HCMode.COOLING
-            and AvailableMode.COOL in self.data.available_modes
-        ):
-            return HVAC_MODE_COOL
-        else:
-            return HVAC_MODE_HEAT
-
-    @property
-    def hvac_modes(self):
-        """Return the list of available operation modes."""
-        return self._hvac_modes
-
-    @property
-    def device_info(self):
-        return {
-            "identifiers": {("heatmiser neoStat", self._neostat.name)},
-            "name": self._neostat.name,
-            "manufacturer": "Heatmiser",
-            "model": "neoStat",
-            "suggested_area": self._neostat.name,
-        }
-
-    async def async_set_temperature(self, **kwargs):
-        """Set new target temperature."""
-        _LOGGER.info(f"{self.name} : Executing set_temperature() with: {kwargs}")
-        _LOGGER.debug(f"self.data: {self.data}")
-
-        low_temp = kwargs.get(ATTR_TEMPERATURE) or kwargs.get(ATTR_TARGET_TEMP_LOW)
-        high_temp = kwargs.get(ATTR_TARGET_TEMP_HIGH)
-
-        set_target_temperature_task = asyncio.create_task(
-            self._neostat.set_target_temperature(low_temp)
-        )
-        response = await set_target_temperature_task
-        if response:
-            _LOGGER.info(
-                f"{self.name} : Called set_target_temperature with: {low_temp} (response: {response})"
-            )
-
-        set_target_cool_temperature_task = asyncio.create_task(
-            self._neostat.set_cool_temp(high_temp)
-        )
-        response = await set_target_cool_temperature_task
-        if response:
-            _LOGGER.info(
-                f"{self.name} : Called set_cool_temp with: {high_temp} (response: {response})"
-            )
-
-        # The change of target temperature may trigger a change in the current hvac_action
-        # so we schedule a refresh to get new data asap.
-        await self._coordinator.async_request_refresh()
+        neo_devices = {device.name: device for device in devices['neo_devices']}
+        return neo_devices[self.name]
 
     async def async_set_hvac_mode(self, hvac_mode):
         """Set hvac mode."""
@@ -329,11 +188,127 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
             f"{self.name} : Called set_frost() with: {frost} (response: {response})"
         )
 
+    async def async_set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        _LOGGER.info(f"{self.name} : Executing set_temperature() with: {kwargs}")
+        _LOGGER.debug(f"self.data: {self.data}")
+
+        low_temp = kwargs.get(ATTR_TEMPERATURE) or kwargs.get(ATTR_TARGET_TEMP_LOW)
+        high_temp = kwargs.get(ATTR_TARGET_TEMP_HIGH)
+
+        set_target_temperature_task = asyncio.create_task(
+            self._neostat.set_target_temperature(low_temp)
+        )
+        response = await set_target_temperature_task
+        if response:
+            _LOGGER.info(
+                f"{self.name} : Called set_target_temperature with: {low_temp} (response: {response})"
+            )
+
+        set_target_cool_temperature_task = asyncio.create_task(
+            self._neostat.set_cool_temp(high_temp)
+        )
+        response = await set_target_cool_temperature_task
+        if response:
+            _LOGGER.info(
+                f"{self.name} : Called set_cool_temp with: {high_temp} (response: {response})"
+            )
+
+        # The change of target temperature may trigger a change in the current hvac_action
+        # so we schedule a refresh to get new data asap.
+        await self._coordinator.async_request_refresh()
+
+    @property
+    def available(self):
+        """Return true if the entity is available."""
+        if self.data.offline:
+            return False
+        return True
+
+    @property
+    def current_temperature(self):
+        """Returns the current temperature."""
+        if self.data.offline:
+            return None
+        return float(self.data.temperature)
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {("Heatmiser Neo Device", self._neostat.device_id)},
+            "name": self._neostat.name,
+            "manufacturer": "Heatmiser",
+            "model": f"Device Type: {self._neostat.device_type}",
+            "suggested_area": self._neostat.name,
+            "sw_version": self.data.stat_version
+        }
+
+    @property
+    def extra_state_attributes(self):
+        """Return the additional state attributes."""
+        attributes = OrderedDict()
+
+        attributes['device_type'] = self.data.device_type
+        attributes['low_battery'] = self.data.low_battery
+        attributes['offline'] = self.data.offline
+        attributes['standby'] = self.data.standby
+        attributes['hold_on'] = self.data.hold_on
+        attributes['hold_time'] = ':'.join(str(self.data.hold_time).split(':')[:2])
+        attributes['hold_temp'] = self.data.hold_temp
+        attributes['floor_temperature'] = self.data.current_floor_temperature
+        attributes['preheat_active'] = self.data.preheat_active
+        attributes['hc_mode'] = self.data.hc_mode
+        attributes['sensor_mode'] = self.data.sensor_mode
+
+        return attributes
+
+    @property
+    def name(self):
+        """Returns the name."""
+        return self._neostat.name
+
+    @property
+    def hvac_action(self):
+        # See: https://developers.home-assistant.io/docs/core/entity/climate/
+        """The current HVAC action (heating, cooling)"""
+        if self.data.standby:
+            return HVACAction.OFF
+        elif self.data.preheat_active:
+            return HVACAction.PREHEATING
+        elif self.data.cool_on:
+            return HVACAction.COOLING
+        elif self.data.heat_on:
+            return HVACAction.HEATING
+        elif self.data.fan_speed != "Off":
+            return HVACAction.FAN  # Should fan be combined? Ie can you have fan on and other functions together?
+        else:
+            return HVACAction.IDLE
+
+    @property
+    def hvac_mode(self):
+        """Return The current operation (e.g. heat, cool, idle). Used to determine state."""
+        if self.data.standby:
+            return HVACMode.OFF
+        elif self.data.hc_mode == "COOLING":
+            return HVACMode.COOL
+        elif self.data.hc_mode == "HEATING":
+            return HVACMode.HEAT
+        elif self.data.hc_mode == "AUTO":
+            return HVACMode.HEAT_COOL
+        elif self.data.hc_mode == "VENT":
+            return HVACMode.FAN_ONLY
+
+    @property
+    def hvac_modes(self):
+        """Return the list of available operation modes."""
+        return self._hvac_modes
+
     async def set_hold(self, hold_duration: object, hold_temperature: int):
         """
         Sets Hold for Zone
         """
-        _LOGGER.warning(f"{self.name} : Executing set_hold() with duration: {hold_duration}, temperature: {hold_temperature}")
+        _LOGGER.warning(
+            f"{self.name} : Executing set_hold() with duration: {hold_duration}, temperature: {hold_temperature}")
         _LOGGER.debug(f"self.data: {self.data}")
 
         hold_hours = 0
@@ -354,7 +329,6 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
             hold_hours = int(hold_duration)
             _LOGGER.debug(f"{self.name} : Duration interpreted from number")
 
-
         if hold_minutes > 59:
             _hold_revised_minutes = hold_minutes % 60
             hold_hours += int((hold_minutes - _hold_revised_minutes) / 60)
@@ -362,7 +336,8 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
         if hold_hours > 99:
             hold_hours = 99
 
-        message = {"HOLD": [{"temp":hold_temperature, "hours":hold_hours, "minutes":hold_minutes, "id":self.name}, [self.name]]}
+        message = {"HOLD": [{"temp": hold_temperature, "hours": hold_hours, "minutes": hold_minutes, "id": self.name},
+                            [self.name]]}
         reply = {"result": "temperature on hold"}
 
         result = await self._hub._send(message, reply)
@@ -377,20 +352,76 @@ class NeoStatEntity(CoordinatorEntity, ClimateEntity):
 
         return result
 
+    @property
+    def should_poll(self):
+        """Don't poll - we fetch the data from the hub all at once"""
+        return False
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        hvac_mode = self.hvac_mode
+        if hvac_mode == HVAC_MODE_HEAT:
+            return SUPPORT_FLAGS | SUPPORT_TARGET_TEMPERATURE
+        elif hvac_mode == HVAC_MODE_COOL:
+            return SUPPORT_FLAGS | SUPPORT_TARGET_TEMPERATURE
+        elif hvac_mode == HVAC_MODE_OFF:
+            return SUPPORT_FLAGS
+        elif hvac_mode == HVAC_MODE_HEAT_COOL:
+            return SUPPORT_FLAGS | SUPPORT_TARGET_TEMPERATURE_RANGE
+        elif hvac_mode == HVAC_MODE_FAN_ONLY:
+            return SUPPORT_FLAGS | SUPPORT_TARGET_TEMPERATURE
+        else:
+            _LOGGER.error(f"Unsupported hvac mode: {hvac_mode}")
+            return SUPPORT_FLAGS
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        return float(self.data.target_temperature)
+
+    @property
+    def target_temperature_high(self):
+        """Return the temperature we try to reach."""
+        return float(self.data.cool_temp)
+
+    @property
+    def target_temperature_low(self):
+        """Return the temperature we try to reach."""
+        return float(self.data.target_temperature)
+
+    @property
+    def target_temperature_step(self):
+        """Return the supported step of target temperature."""
+        # TODO: Step for V1 and V2 varies?
+        return self._target_temperature_step
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement."""
+        if self._unit_of_measurement == "C":
+            return TEMP_CELSIUS
+        if self._unit_of_measurement == "F":
+            return TEMP_FAHRENHEIT
+        return self._unit_of_measurement
+
+    @property
+    def unique_id(self):
+        """Return a unique ID."""
+        return f"{self._neostat.device_id}_neostat"
 
     async def unset_hold(self):
         """
         Unsets Hold for Zone
         """
 
-        message = {"HOLD": [{"temp":20, "hours":0, "minutes":0, "id":self.name}, [self.name]]}
+        message = {"HOLD": [{"temp": 20, "hours": 0, "minutes": 0, "id": self.name}, [self.name]]}
         reply = {"result": "temperature on hold"}
 
         result = await self._hub._send(message, reply)
 
         # Optimistically update the mode so that the UI feels snappy.
         # The value will be confirmed next time we get new data.
-
         self.data.hold_on = False
         self.data.hold_time = str("0:00")
         self.data.hold_temp = 20

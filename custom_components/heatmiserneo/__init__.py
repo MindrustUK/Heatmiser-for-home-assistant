b38f0c4330adb88c81dbd754cd32b1857739562f
MindrustUK <mehmetcan@mindrust.co.uk>
2023-11-22 09:44:48 +0000

    Update __init__.py

--- custom_components/heatmiserneo/__init__.py
+++ custom_components/heatmiserneo/__init__.py
@@ -93,10 +93,6 @@ async def async_setup_entry(hass, entry):
     )
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "select")
-    )
-
-    hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
 

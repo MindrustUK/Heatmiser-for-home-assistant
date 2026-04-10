3635913d36610057407d83ea171af2d8e66b8c6b
ocrease <18347739+ocrease@users.noreply.github.com>
2026-04-10 08:06:11 +0100

    Fix Issue with timezone GMT+10

--- custom_components/heatmiserneo/select.py
+++ custom_components/heatmiserneo/select.py
@@ -495,7 +495,7 @@ HUB_SELECT: Final[tuple[HeatmiserNeoHubSelectEntityDescription, ...]] = (
             "tz875",
             "tz900",
             "tz950",
-            "tz100",
+            "tz1000",
             "tz1050",
             "tz1100",
             "tz1200",
